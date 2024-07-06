508af9e349233f20991acf5e304186b3124a55c5
iamukn <n.u.kingsley@gmail.com>
2024-07-06 23:38:51 +0100

    tested the babel transpiler

---
+++ babel_compiled/name.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function greet() {
+  //console.log('Hello world!');
+  return 1;
+}
+greet();
+var sum = function sum(x) {
+  x + 2;
+};
+sum(2);
+var add = function add(x, y) {
+  x + y;
+};
+add();
--- eslint_play/name.js
+++ eslint_play/name.js
@@ -8,3 +8,10 @@ greet();
 
 let sum = x =>  {x + 2;};
 sum(2);
+
+
+const add = (x, y) => {
+  x + y;
+};
+add();
+
