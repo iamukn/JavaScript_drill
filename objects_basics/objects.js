b143878907c5e2691f15430f41396ff0306c48d1
iamukn <n.u.kingsley@gmail.com>
2024-07-07 20:30:09 +0100

    using the delete keyword to deleted a key and its value from an object

--- objects_basics/objects.js
+++ objects_basics/objects.js
@@ -2,3 +2,15 @@
 
 let user = new Object();
 console.log(typeof user);
+
+name = 'fullname';
+
+let newUser  = {
+    [name]: "Ukaegbu",
+    age: 30,
+};
+
+
+console.log(newUser);
+delete newUser.age;
+console.log(newUser);
